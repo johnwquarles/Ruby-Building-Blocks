6fb7bc1802e959341388bf8510f0c2ef27043f64
johnwquarles <john.w.quarles@gmail.com>
2014-11-19 01:58:10 +0900

    stockpicker works

--- stockpicker.rb
+++ stockpicker.rb
@@ -1,25 +1,25 @@
 def stockPicker(array)
+  return "Please input a chronological array of stock prices" unless array.is_a?Array
   candidates = []
-  array.each do |buyPrice|
-    buyDay = array.index(buyPrice)
-    array[buyDay+1, array[-1]].each do |sellPrice|
-      sellDay = array.index(sellPrice)
+  (0..(array.length - 1)).each do |buyDay|
+    buyPrice = array[buyDay]
+    ((buyDay + 1)..(array.length - 1)).each do |sellDay|
+      sellPrice = array[sellDay]
       profit = sellPrice - buyPrice
       candidates << [buyDay, sellDay, buyPrice, sellPrice, profit]
     end
   end
 
-return candidates
-  
 bestProfit = 0
 bestCandidate = []
 candidates.each do |candidate|
   if candidate[4] > bestProfit
+    bestProfit = candidate[4]
     bestCandidate = candidate
   end
 end
 
-return "No profits to be had" if bestCandidate == []
+return "No profits to be had." if bestCandidate == []
 return "I'd recommend buying on day #{bestCandidate[0]+1} and selling on day #{bestCandidate[1]+1}," + " for a profit of $#{bestCandidate[3]} - $#{bestCandidate[2]} = $#{bestCandidate[4]}."
 
 end
